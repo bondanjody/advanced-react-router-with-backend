93ca8adbb8c2716b3ce7cf6a821b1271e23049b0
bondanjody <bondanjodys@gmail.com>
2023-03-28 02:39:40 +0700

    Mengekstrak data error dan menthrow Response. Menambahkan komponen PageContent yang nantinya digunakan untuk menampung tampilan error.

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,5 +1,28 @@
+import { useRouteError } from "react-router-dom";
+import PageContent from "../components/PageContent";
+import MainNavigation from "../components/MainNavigation";
+
 function ErrorPage() {
-    return <h1>An ErrorPage occured !</h1>
+    const err = useRouteError();
+
+    let title = 'An error occured';
+    let message = 'Something went wrong !';
+
+    if (err.status === 500) {
+        message = JSON.parse(err.data).message;
+    }
+
+    if (err.status === 400) {
+        title = 'Not Found'
+        message = 'Could not find resource or page !';
+    }
+    return <>
+        <MainNavigation />
+        <PageContent title={title}>
+            <p>{message}</p>
+        </PageContent>
+    </>
+    
 }
 
 export default ErrorPage;
