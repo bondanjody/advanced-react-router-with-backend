0f59268871f0247f6dcd6924b3d163897dff82fc
bondanjody <bondanjodys@gmail.com>
2023-03-27 15:58:31 +0700

    Menambahkan useParams untuk mengakses dynamic parameter pada URL.

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,5 +1,13 @@
+import { Fragment } from "react";
+import { useParams } from "react-router-dom";
+
 const EventDetail = () => {
-    return <h1>EventDetail</h1>;
+    const params = useParams();
+
+    return <Fragment>
+        <h1>EventDetail</h1>
+        <p>{params.eventID}</p>
+    </Fragment>;
 }
 
 export default EventDetail;
