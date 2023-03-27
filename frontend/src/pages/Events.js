b0f59e62b3702a1e6b5073a990b2b2417cf07bd4
bondanjody <bondanjodys@gmail.com>
2023-03-28 02:11:57 +0700

    Menambahkan NOTE penjelasan tentang apa saja yang dapat dituliskan di loader()

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -18,4 +18,10 @@ export async function loader() {
   } else {
     return response;
   }
+
+  /*
+    NOTE :
+    Semua kode API bawaan Browser (seperti : localStorage, cookie, dll) dapat dituliskan pada loader().
+    Yang tidak bisa dituliskan pada loader() adalah kode React seperti : useState, dll.
+  */
 }
