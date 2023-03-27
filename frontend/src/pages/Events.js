cfbbcc99874a5d5d661ee36eac757e865c133395
bondanjody <bondanjodys@gmail.com>
2023-03-27 15:55:37 +0700

    Menambahkan dummy data untuk event. Serta membuat link untuk menuju masing-masing event

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,5 +1,19 @@
+import { Fragment } from "react";
+import { Link } from "react-router-dom";
+
 const Events = () => {
-    return <h1>Events</h1>;
+    const DUMMY_DATA = [
+        {id: 'event1', title: 'Jakarta Music Fest'},
+        {id: 'event2', title: 'Surakarta Soundrenaline'}
+    ]
+
+    return <Fragment>
+        <ul>
+        {DUMMY_DATA.map(data => <li key={data.id}>
+            <Link to={data.id}>{data.title}</Link>
+        </li>)}
+        </ul>
+    </Fragment>;
 }
 
 export default Events;
