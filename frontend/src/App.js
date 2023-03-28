c25f7f70c7df8c8f12925ef6173d348c410ff742
bondanjody <bondanjodys@gmail.com>
2023-03-28 14:10:52 +0700

    Dynamic Routes dan loader()

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import {RouterProvider, createBrowserRouter} from 'react-router-dom';
 import Home from './pages/Home';
 import Events, {loader as eventsLoader} from './pages/Events';
-import EventDetail from './pages/EventDetail';
+import EventDetail, { loader as eventDetailLoader } from './pages/EventDetail';
 import NewEvent from './pages/NewEvent';
 import EditEvent from './pages/EditEvent';
 import RootLayout from './pages/Root'
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
     {index: true, element: <Home />},
     {path: 'events', element: <EventsRootLayout />, children: [
       {index: true, element: <Events />, loader: eventsLoader },
-      {path: ':eventID', element: <EventDetail />},
+      {path: ':eventID', element: <EventDetail />, loader: eventDetailLoader},
       {path: 'new', element: <NewEvent />},
       {path: ':eventID/edit', element: <EditEvent />},
     ]},
--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,13 +1,25 @@
 import { Fragment } from "react";
-import { useParams } from "react-router-dom";
+import { useLoaderData, json } from "react-router-dom";
+import EventItem from '../components/EventItem';
 
 const EventDetail = () => {
-    const params = useParams();
+    const data = useLoaderData();
 
     return <Fragment>
-        <h1>EventDetail</h1>
-        <p>{params.eventID}</p>
+        <EventItem event={data.event} />
     </Fragment>;
 }
 
 export default EventDetail;
+
+export async function loader({request, params}) {
+    const id = params.eventID;
+
+    const response = await fetch('http://localhost:8080/events/' + id );
+
+    if (!response.ok) {
+        throw json({ message: 'Could not fetch details for selected events !'}, { status: 500});
+    } else {
+        return response;
+    }
+}
